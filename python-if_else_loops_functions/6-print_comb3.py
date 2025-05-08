1f8d5a855167dbbd13f05584325f9e958bbec072
warrre <warren.soulsey@gmail.com>
2025-05-09 00:19:05 +0200

    Refactor 6-print_comb3.py to simplify conditionals for printing combinations of two digits

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for i in range(9):
     for j in range(i + 1, 10):
-        if i != j:
-            if i != 8 or j != 9:
-                print("{:d}{:d}".format(i, j), end=", ")
-            else:
-                print("{:d}{:d}".format(i, j))
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")
