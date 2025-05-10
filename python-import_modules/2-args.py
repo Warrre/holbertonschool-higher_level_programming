58b11d3eb16073f10eeec73bdc7ecc759bbfa74a
warrre <warren.soulsey@gmail.com>
2025-05-10 21:18:45 +0200

    Refactor 2-args.py to simplify argument handling and improve code clarity

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,14 +2,11 @@
 import sys
 
 if __name__ == "__main__":
-    argv = sys.argv
-    argc = len(argv) - 1
-    if argc == 0:
+    if len(sys.argv) > 2:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv) - 1))
+    elif len(sys.argv) == 1:
         print("0 arguments.")
-    elif argc == 1:
-        print("1 argument:")
-    elif argc > 1:
-        print("{} arguments:".format(argc))
-    if argc > 0:
-        for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
