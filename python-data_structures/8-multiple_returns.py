8cd2ca45f353efb73d36e8a1e900b2241fa13a5f
warrre <warren.soulsey@gmail.com>
2025-05-14 18:41:07 +0200

    Implement functions for checking divisibility, deleting elements, and finding maximum values in lists

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,11 @@
-def multiple_returns(sentence):
+#!/usr/bin/python3
 
-    if len(sentence) == 0:
-        return (0, None)
+def multiple_returns(sentence):
+    char = 0
+    length = len(sentence)
+
+    if sentence == "":
+        char = None
     else:
-        return (len(sentence), sentence[0])
+        char = sentence[0]
+    return (length, char)
