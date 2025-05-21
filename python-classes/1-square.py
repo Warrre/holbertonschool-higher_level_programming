48a94730c260574662d29ee7d6fe99fbb944d823
warrre <warren.soulsey@gmail.com>
2025-05-21 15:18:40 +0200

    Remove unnecessary blank lines in Square class definition

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,12 +2,10 @@
 """
 Classe Square qui définit un carré par sa taille (size)
 """
-
 class Square:
     """
     Classe représentant un carré
     """
-
     def __init__(self, size):
         """
         Constructeur de la classe Square
