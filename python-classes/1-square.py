7aec9cd9795e965661ec7203d66652bd0e1b5aab
warrre <warren.soulsey@gmail.com>
2025-05-21 11:15:00 +0200

    Add Square class with size and position attributes, including validation and print method

---
+++ python-classes/1-square.py
@@ -0,0 +1,4 @@
+class Square:
+    """Classe qui définit un carré avec une taille privée."""
+    def __init__(self, size):
+        self.__size = size
---
+++ python-classes/2-square.py
@@ -0,0 +1,8 @@
+class Square:
+    """Classe qui valide la taille d’un carré."""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
