fb1d31c2a4bb93b86277ebb1cb0f48de6b0c3f38
warrre <warren.soulsey@gmail.com>
2025-06-06 19:32:36 +0200

    Refactor read_file function docstring and ensure consistent file opening mode

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
+"""Module for read_file"""
 
 
 def read_file(filename=""):
-    """Reads a text file and prints its content to stdout.
-    """
-    with open(filename, encoding="utf-8") as f:
+    """Read a text file (UTF8) and prints it to stdout"""
+
+    with open(filename, "r", encoding='utf-8') as f:
         print(f.read(), end="")
