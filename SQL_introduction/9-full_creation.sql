932a5f132b468ecbcd4f24204e6ad3006831826b
warrre <warren.soulsey@gmail.com>
2025-06-25 11:09:27 +0200

    Add SQL scripts for database and table operations, including creation, insertion, selection, and deletion

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table (id, name, score) VALUES
+(10, 'War', 100),
+(20, 'Goku', 30),
+(30, 'Luffy', 140),
+(40, 'Asta', 80);
