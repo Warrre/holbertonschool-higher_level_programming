5c20932a9df27873271e2f4ae5e431a3947b7a05
warrre <warren.soulsey@gmail.com>
2025-06-27 23:37:41 +0200

    Refactor 0-select_states.py to improve docstring clarity and streamline database connection code

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,23 @@
 #!/usr/bin/python3
 """
-Script qui liste tous les états d'une base de données.
+that lists all states from the
+database hbtn_0e_0_usa
 """
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    # Récupération des arguments de la ligne de commande
-    username = sys.argv[1]  # Nom d'utilisateur MySQL
-    password = sys.argv[2]  # Mot de passe MySQL
-    db_name = sys.argv[3]   # Nom de la base de données
-
-    # Établissement de la connexion à la base de données
-    db = MySQLdb.connect(
-        host="localhost",      # Adresse du serveur MySQL
-        port=3306,             # Port par défaut
-        user=username,
-        passwd=password,
-        db=db_name
+    db_connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
-
-    # Création du curseur et exécution de la requête
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Récupération et affichage des résultats
-    results = cursor.fetchall()
-    for row in results:
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+    for row in cursor.fetchall():
         print(row)
-
-    # Fermeture des ressources
     cursor.close()
+    db_connection.close()
