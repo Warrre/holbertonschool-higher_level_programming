50d21f2fa4792bfb4228d4e759e4fa556866c688
warrre <warren.soulsey@gmail.com>
2025-06-27 23:56:49 +0200

    Refactor 7-model_state_fetch_all.py to improve docstring clarity and streamline code formatting

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 """
-Script qui liste tous les objets State de la base de données.
+7-model_state_fetch_all
+that lists all State objects
+from the database hbtn_0e_6_usa
+and is ordered by id (ascending)
 """
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+import sys
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    states = session.query(State).order_by(State.id).all()
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
-
+    states = session.query(State).order_by(State.id)
+    for row in states:
+        print("{}: {}".format(row.id, row.name))
     session.close()
